bff0123e3b747f7554adda894788f9f46517fca9
Alok Acharya <alokacharya123@hotmail.com>
2024-04-16 18:53:25 -0400

    Added webpage for the main project screen
    Added index.html for the project screen, javascript for the overlay animation, and styles

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let projectItems = document.getElementsByClassName('projects__list-item');
+
+for (let i = 0; i < projectItems.length; i++) {
+	let projectItem = projectItems[i];
+	console.log(projectItem);
+	projectItem.addEventListener('mouseenter', (event) => {
+		let overlay = projectItem.getElementsByClassName(
+			'projects__list-overlay'
+		)[0];
+		overlay.classList.toggle('projects__list-overlay--active');
+	});
+
+	projectItem.addEventListener('mouseleave', () => {
+		let overlay = projectItem.getElementsByClassName(
+			'projects__list-overlay'
+		)[0];
+		overlay.classList.toggle('projects__list-overlay--active');
+	});
+}
